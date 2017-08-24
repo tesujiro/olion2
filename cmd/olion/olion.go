9be221bd5cd97e248f238ab0082434f61a2bd451
tesujiro <tesujiro@gmail.com>
2017-08-25 08:55:23 +0900

    add profiling

--- cmd/olion/olion.go
+++ cmd/olion/olion.go
@@ -1,7 +1,47 @@
 package main
 
-import "github.com/tesujiro/olion"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+
+	"github.com/tesujiro/olion"
+)
 
 func main() {
-	olion.Run()
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error:\n%s", err)
+			os.Exit(1)
+		}
+	}()
+	os.Exit(_main())
+}
+
+func _main() int {
+	if envvar := os.Getenv("GOMAXPROCS"); envvar == "" {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cpuprofile := "mycpu.prof"
+	f, err := os.Create(cpuprofile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	olion := olion.New()
+
+	if err := olion.Run(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return 1
+	}
+
+	return 0
 }
