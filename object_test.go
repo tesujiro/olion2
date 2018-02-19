e34dfd8db4c5347d7f0a6c7b88bef7356eeee16b
tesujiro <tesujiro@gmail.com>
2018-02-19 21:06:08 +0900

    add: object test

--- object_test.go
+++ object_test.go
@@ -26,3 +26,25 @@ func TestStar(t *testing.T) {
 		}
 	}
 }
+
+func TestBomb(t *testing.T) {
+	cases := []struct {
+		size            int
+		position, speed Coordinates
+		expectedSpeed   Coordinates
+		expectedParts   int
+	}{
+		{size: 10, position: Coordinates{10, 20, 30}, speed: Coordinates{1, 2, 3}, expectedSpeed: Coordinates{-1, -2, -3}, expectedParts: 1},
+	}
+	for _, c := range cases {
+		o := newBomb(time.Now(), c.size, c.position, c.speed)
+		actualSpeed := o.getSpeed()
+		if actualSpeed != c.expectedSpeed {
+			t.Errorf("got %v\nwant %v", actualSpeed, c.expectedSpeed)
+		}
+		actualParts := len(o.getParts())
+		if actualParts != c.expectedParts {
+			t.Errorf("got %v\nwant %v", actualParts, c.expectedParts)
+		}
+	}
+}
