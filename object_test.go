93abbf15291bb5b01ea0cd9d1a47b7632ac0ead8
tesujiro <tesujiro@gmail.com>
2018-02-20 02:59:16 +0900

    add: object test

--- object_test.go
+++ object_test.go
@@ -48,3 +48,28 @@ func TestBomb(t *testing.T) {
 		}
 	}
 }
+
+func TestSpaceShip(t *testing.T) {
+	cases := []struct {
+		size                               int
+		position                           Coordinates
+		expectedSpeedMin, expectedSpeedMax int
+		expectedParts                      int
+	}{
+		{size: 10, position: Coordinates{10, 20, 30}, expectedSpeedMin: 0, expectedSpeedMax: 40, expectedParts: 10},
+	}
+	for _, c := range cases {
+		o := newSpaceShip(time.Now(), c.size, c.position)
+		actualSpeed := o.getSpeed()
+		if actualSpeed.Z < c.expectedSpeedMin {
+			t.Errorf("got %v\nwant min:%v", actualSpeed, c.expectedSpeedMin)
+		}
+		if c.expectedSpeedMax < actualSpeed.Z {
+			t.Errorf("got %v\nwant max:%v", actualSpeed, c.expectedSpeedMax)
+		}
+		actualParts := len(o.getParts())
+		if actualParts != c.expectedParts {
+			t.Errorf("got %v\nwant %v", actualParts, c.expectedParts)
+		}
+	}
+}
